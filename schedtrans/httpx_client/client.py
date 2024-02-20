e3c76f78a9db91e6599dc3670fe0051e884e40fa
TurtleOld <dev@pavlovteam.ru>
2024-02-20 11:24:41 +0300

    create httpx client

---
+++ schedtrans/httpx_client/client.py
@@ -0,0 +1,11 @@
+import httpx
+
+
+async def make_request(url, params):
+    async with httpx.AsyncClient(
+        base_url='https://api.rasp.yandex.net/v3.0/',
+        http2=True,
+        params=params,
+        timeout=10,
+    ) as client:
+        return await client.get(f'/{url}')
