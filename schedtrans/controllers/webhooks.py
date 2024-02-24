6ef41a138642644a03a6bec66c107111eaaf921d
TurtleOld <dev@pavlovteam.ru>
2024-02-24 18:15:49 +0300

    added webhooks

--- schedtrans/controllers/webhooks.py
+++ schedtrans/controllers/webhooks.py
@@ -1,9 +1,14 @@
-from blacksheep import json
+import json
 from blacksheep.server.controllers import Controller, post
-from icecream import ic
+from telebot import types
+
+from schedtrans.telegram.config import bot
 
 
 class WebhookController(Controller):
-    @post('/webhooks')
-    def webhooks(self, request):
-        return request.method
+    @post('/webhooks/')
+    def webhooks(self, request) -> None:
+        if request.method == 'POST':
+            json_data = json.loads(request.body)
+            update = types.Update.de_json(json_data)
+            bot.process_new_updates([update])
