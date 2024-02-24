1214535c803e0c2c8701b8f9f12eeac522bf7a9e
TurtleOld <dev@pavlovteam.ru>
2024-02-24 19:13:04 +0300

    fix webhooks

--- schedtrans/controllers/webhooks.py
+++ schedtrans/controllers/webhooks.py
@@ -8,7 +8,6 @@ from schedtrans.telegram.config import bot
 
 @post('/webhooks')
 async def webhooks(request: Request) -> None:
-    if request.method == 'POST':
-        json_data = await request.json()
-        update = types.Update.de_json(json_data)
-        asyncio.ensure_future(bot.process_new_updates([update]))
+    json_data = await request.json()
+    update = types.Update.de_json(json_data)
+    asyncio.ensure_future(bot.process_new_updates([update]))
