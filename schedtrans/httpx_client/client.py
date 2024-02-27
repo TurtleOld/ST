05456016f7416044d16054a6520bb722debe52fd
TurtleOld <dev@pavlovteam.ru>
2024-02-27 13:21:51 +0300

    refactoring

--- schedtrans/httpx_client/client.py
+++ schedtrans/httpx_client/client.py
@@ -4,7 +4,6 @@ import httpx
 async def make_request(url, params):
     async with httpx.AsyncClient(
         base_url='https://api.rasp.yandex.net/v3.0/',
-        http2=True,
         params=params,
         timeout=10,
     ) as client:
